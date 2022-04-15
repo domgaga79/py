06e42ef66180a410ade35c42abe1b03fd295c93d
Iuri Garcia <103457578+domgaga79@users.noreply.github.com>
2022-04-15 11:04:49 -0300

    Update conectar_mysql.py

--- conectar_mysql.py
+++ conectar_mysql.py
@@ -1,13 +1,16 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host: "localhost";
-    user: "root";
-    password: "";
-    database: "bd_python"
+    host= "localhost";
+    user= "root";
+    password= "";
+    database= "bd_python"
 )
 
-mydb = mysql.execute("SELECT * FROM admin")
 
-for x in rows(mydb):
-    print(x)
+print (mydb)
+
+#mydb = mysql.execute("SELECT * FROM admin")
+
+#for x in rows(mydb):
+#    print(x)
